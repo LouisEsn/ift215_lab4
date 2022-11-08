e1085951a400bf14f066dc521c0ab2941aa71586
LouisEsn <esnl0701@usherbrooke.ca>
2022-11-08 10:55:14 -0500

    Initial project version

---
+++ client/scripts/main.js
@@ -0,0 +1,9 @@
+/*function clicMenuGauche(lien){
+    lien.style.color = "#F00";
+    lien.innerText += " cliqué!"
+}*/
+
+function clicMenuGauche(lien){
+    lien.style.color = "#F00";
+    lien.innerText += " cliqué!"
+}
